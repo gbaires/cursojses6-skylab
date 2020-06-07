b9ae85afb74d21053e4a9dfa78079721fb7f6cc8
Gabriel Aires <gabriel.aires.pt@gmail.com>
2020-06-07 20:45:17 -0300

    Classes

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-alert('Hello!');
+class List {
+	constructor() {
+		this.data = [];
+	}
+
+	add(data) {
+		this.data.push(data);
+		console.log(this.data);
+	}
+}
+
+class TodoList extends List {
+	constructor() {
+		super();
+		this.usuario = 'Diego';
+	}
+
+	mostraUsuario() {
+		console.log(this.usuario);
+	}
+}
+
+class Matematica {
+	static soma(a, b) {
+		return a + b;
+	}
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+	MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+console.log(Matematica.soma(3, 4));
