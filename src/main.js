d3e3a26b02693d2177bc8def3373378f57a43b86
Gabriel Aires <gabriel.aires.pt@gmail.com>
2020-06-08 14:25:15 -0300

    Async/await + axios

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-const minhaPromise = () => new Promise((resolve, reject) => 
-	setTimeout(() => { resolve('OK') }, 2000)
-)
+import axios from 'axios';
 
-//minhaPromise().then(response => ...);
+class Api {
+	static async getUserInfo(username) {
+		try {
+			const response = await axios.get(`https://api.github.com/users/${username}`);
 
-/*async function executaPromise() {
-	console.log(await minhaPromise());
-	console.log(await minhaPromise());
-	console.log(await minhaPromise());
-}*/
+			console.log(response);
+		} catch(err) {
+			console.warn('Erro na API');
+		}
+	}
+}
 
-const executaPromise = async () => {
-	console.log(await minhaPromise());
-	console.log(await minhaPromise());
-	console.log(await minhaPromise());
-};
-
-executaPromise();
+Api.getUserInfo('gbaires');
+Api.getUserInfo('gbaires178273231993273');
